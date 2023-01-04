a1b26a2ddaf5b5f1775dfe383ccee24d0b999487
JordanVL1234 <96630742+JordanVL1234@users.noreply.github.com>
2023-01-04 18:12:17 -0500

    Add files via upload

--- PassGen.py
+++ PassGen.py
@@ -1,35 +1,26 @@
 import random
 import json
 
-## What's the password for? ##
 vuse = input("What's this password for? - ")
 
-## PassGen ##
 vabc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 v123 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 vsign = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+", "="]
-vv = [vabc, v123, vsign]
+vbc = [vabc, v123, vsign]
+vnum = 0
+vbpass = []
 
-## PassGen ##
-v1 = random.choice(vv)
-c1 = random.choice(v1)
-v2 = random.choice(vv)
-c2 = random.choice(v2)
-v3 = random.choice(vv)
-c3 = random.choice(v3)
-v4 = random.choice(vv)
-c4 = random.choice(v4)
-v5 = random.choice(vv)
-c5 = random.choice(v5)
-v6 = random.choice(vv)
-c6 = random.choice(v6)
-v7 = random.choice(vv)
-c7 = random.choice(v7)
-v8 = random.choice(vv)
-c8 = random.choice(v8)
+while vnum < 8:
+        vv = random.choice(vbc)
+        vc = random.choice(vv)
+        vbpass.append(vc)
+        vnum = vnum+1
 
-## The Output ##
-vpass = [c1, c2, c3, c4, c5, c6, c7, c8]
-with open("Password.json", "w") as f:
-        json.dumps([vuse, " - ", vpass])
-print("Your password for ", vuse, " is - ", *vpass, sep='')
+vapass = ''.join(map(str, vbpass))
+
+if vuse == "":
+        print("Your password is -", vapass)
+else:
+        with open("Passwords.json", "a") as f:
+                f.write(json.dumps("{} - {}".format(vuse, vapass)))
+        print("Your password for", vuse, "is -", vapass)
